315bf2dd1c5527521be2e5dcd0e46110affc9ab1
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-10-05 16:20:54 +0300

    Added functions for deleting tickets and JSX for delete confirmation

--- src/components/Tickets/TicketList/TicketItem.js
+++ src/components/Tickets/TicketList/TicketItem.js
@@ -1,25 +1,46 @@
+import { useState } from 'react';
+
 import Card from '../../UI/Card';
 import Button from '../../UI/Button';
 import classes from './TicketItem.module.css';
 
-const TicketItem = ({ title, description, reporter, assignee, date, priority }) => {
+const TicketItem = (props) => {
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    const toggleConfirmDelete = () => {
+        setIsDeleting(!isDeleting ? true : false);
+    };
+
+    const deleteTicket = () => {
+        props.onDelete(props.id);
+    };
+
     return (
         <li>
             <Card>
-                <div className={classes.ticket}>
+                <div className={`${classes.ticket} ${isDeleting ? classes.blur : ''}`}>
                     <header>
                         <div>
-                            <h3>{title}</h3>
-                            <p>{reporter}</p>
-                            <p>{assignee}</p>
+                            <h3>{props.title}</h3>
+                            <p>{props.reporter}</p>
+                            <p>{props.assignee}</p>
                         </div>
-                        <Button>X</Button>
+                        <Button onClick={toggleConfirmDelete}>X</Button>
                     </header>
-                    <p className={classes.description}>{description}</p>
+                    <p className={classes.description}>{props.description}</p>
                     <footer>
-                        <p>Priority: {priority}</p>
-                        <p>Date: {date}</p>
+                        <p>Priority: {props.priority}</p>
+                        <p>Date: {props.date}</p>
                     </footer>
+                    {isDeleting && 
+                        <div className={classes["confirm-delete"]}>
+                            <p>Are you sure you want to delete this?</p>
+                            <div>
+                                <Button onClick={deleteTicket}>Yes</Button>
+                                <Button onClick={toggleConfirmDelete}>No</Button>
+                            </div>
+                        </div>
+                    }
                 </div>
             </Card>
         </li>

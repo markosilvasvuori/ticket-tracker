6859d7887968f3d35daf07b3426df8effe65e499
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-10-05 16:18:11 +0300

    Added function to toggle ticket form

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,16 @@
 import Button from '../UI/Button';
 import classes from './Header.module.css';
 
-const Header = () => {
+const Header = props => {
+    const toggleForm = (e) => {
+        e.preventDefault();
+        props.onToggle();
+    };
+
     return (
         <header className={classes.header}>
             <h1>Ticket Tracker</h1>
-            <Button>+ Add Ticket</Button>
+            <Button onClick={toggleForm}>+ Add Ticket</Button>
         </header>
     );
 };

862ca6a5402c94430ef5ab092c1ab5a0745c9631
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-10-07 14:58:24 +0300

    Replaced hardcoded array with localStorage

--- src/store/TicketContext.js
+++ src/store/TicketContext.js
@@ -3,48 +3,85 @@ import { createContext, useState } from 'react';
 export const TicketContext = createContext();
 
 export const TicketProvider = props => {
-    const [tickets, setTickets] = useState([
-        {
-            id: 'default1',
-            isSolved: false,
-            title: 'Title',
-            description: 'Description',
-            reporter: 'John Doe',
-            assignee: 'Jane',
-            date: '4.10.2021',
-            priority: 'Low'
-        },
-        {
-            id: 'default2',
-            isSolved: false,
-            title: 'Title 1',
-            description: 'Description',
-            reporter: 'John Doe',
-            assignee: 'Jane',
-            date: '4.10.2021',
-            priority: 'Medium'
-        },
-        {
-            id: 'default3',
-            isSolved: true,
-            title: 'Title 2',
-            description: 'Description',
-            reporter: 'John Doe',
-            assignee: 'Jane',
-            date: '4.10.2021',
-            priority: 'High'
-        },
-        {
-            id: 'default4',
-            isSolved: false,
-            title: 'Title 2',
-            description: 'Description',
-            reporter: 'John Doe',
-            assignee: 'Jane',
-            date: '4.10.2021',
-            priority: 'High'
-        },
-    ]);
+    const storedTickets = JSON.parse(localStorage.getItem('stored-tickets'));
+
+    const checkForLocalStorage = () => {
+        // localStorage.clear();
+        if (storedTickets) {
+            console.log(storedTickets)
+            return storedTickets;
+        } else {
+            return [
+                {
+                    id: 'default1',
+                    isSolved: false,
+                    title: 'Title',
+                    description: 'Description',
+                    reporter: 'John Doe',
+                    assignee: 'Jane',
+                    date: '4.10.2021',
+                    priority: 'Low'
+                },
+                {
+                    id: 'default2',
+                    isSolved: false,
+                    title: 'Title 1',
+                    description: 'Description',
+                    reporter: 'John Doe',
+                    assignee: 'Jane',
+                    date: '4.10.2021',
+                    priority: 'Medium'
+                }
+            ];
+        }
+    };
+
+    const [tickets, setTickets] = useState(
+        checkForLocalStorage()
+    );
+
+    // const [tickets, setTickets] = useState([
+    //     {
+    //         id: 'default1',
+    //         isSolved: false,
+    //         title: 'Title',
+    //         description: 'Description',
+    //         reporter: 'John Doe',
+    //         assignee: 'Jane',
+    //         date: '4.10.2021',
+    //         priority: 'Low'
+    //     },
+    //     {
+    //         id: 'default2',
+    //         isSolved: false,
+    //         title: 'Title 1',
+    //         description: 'Description',
+    //         reporter: 'John Doe',
+    //         assignee: 'Jane',
+    //         date: '4.10.2021',
+    //         priority: 'Medium'
+    //     },
+    //     {
+    //         id: 'default3',
+    //         isSolved: true,
+    //         title: 'Title 2',
+    //         description: 'Description',
+    //         reporter: 'John Doe',
+    //         assignee: 'Jane',
+    //         date: '4.10.2021',
+    //         priority: 'High'
+    //     },
+    //     {
+    //         id: 'default4',
+    //         isSolved: false,
+    //         title: 'Title 2',
+    //         description: 'Description',
+    //         reporter: 'John Doe',
+    //         assignee: 'Jane',
+    //         date: '4.10.2021',
+    //         priority: 'High'
+    //     },
+    // ]);
 
     return (
         <TicketContext.Provider value={[tickets, setTickets]}>

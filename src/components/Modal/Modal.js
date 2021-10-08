d0c1c19662332f5fc5d6379914d5e22855aa2bd9
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-10-08 15:14:40 +0300

    Moved Reporter and Assignee to footer

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -6,13 +6,10 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './Modal.module.css';
 
-const modalRoot = document.querySelector('#modal-root');
-
 const Modal = props => {  
     const [tickets] = useContext(TicketContext);
     const ticket = tickets.filter(ticket => ticket.id === props.ticketID)[0];
-
-    console.log(ticket)
+    const modalRoot = document.querySelector('#modal-root');
 
     const closeModal = () => {
         props.closeModal();
@@ -23,17 +20,27 @@ const Modal = props => {
             <div className={classes.backdrop} onClick={closeModal}></div>
             <Card className={classes["modal-overlay"]}>
                 <header>
-                    <div>
-                        <h3>{ticket.title}</h3>
-                        <p>{ticket.reporter}</p>
-                        <p>{ticket.assignee}</p>
-                    </div>
+                    <h3 style={{color: ticket.priorityColor}}>{ticket.title}</h3>
                     <Button onClick={closeModal}>Close</Button>
                 </header>
                 <p className={classes.description}>{ticket.description}</p>
                 <footer>
-                    <p>Priority: <span style={{color: ticket.priorityColor}}>{ticket.priority}</span></p>
-                    <p>Date: {ticket.date}</p>
+                    <div>
+                        <h4>Reporter:</h4>
+                        <p>{ticket.reporter}</p>
+                    </div>
+                    <div>
+                        <h4>Assignee:</h4>
+                        <p>{ticket.assignee}</p>
+                    </div>
+                    <div>
+                        <h4>Priority:</h4>
+                        <p style={{color: ticket.priorityColor}}>{ticket.priority}</p>
+                    </div>
+                    <div>
+                        <h4>Date:</h4>
+                        <p>{ticket.date}</p>
+                    </div>
                 </footer>
             </Card>
         </Fragment>,

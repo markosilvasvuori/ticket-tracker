94153ef4020e8db01b87cfbad6576e67f19638f8
Marko Silvasvuori <marko.silvasvuori@gmail.com>
2021-10-08 15:21:05 +0300

    Removed unused code

--- src/store/TicketContext.js
+++ src/store/TicketContext.js
@@ -6,9 +6,7 @@ export const TicketProvider = props => {
     const storedTickets = JSON.parse(localStorage.getItem('stored-tickets'));
 
     const checkForLocalStorage = () => {
-        // localStorage.clear();
         if (storedTickets) {
-            console.log(storedTickets)
             return storedTickets;
         } else {
             return [
@@ -41,49 +39,6 @@ export const TicketProvider = props => {
     const [tickets, setTickets] = useState(
         checkForLocalStorage()
     );
-
-    // const [tickets, setTickets] = useState([
-    //     {
-    //         id: 'default1',
-    //         isSolved: false,
-    //         title: 'Title',
-    //         description: 'Description',
-    //         reporter: 'John Doe',
-    //         assignee: 'Jane',
-    //         date: '4.10.2021',
-    //         priority: 'Low'
-    //     },
-    //     {
-    //         id: 'default2',
-    //         isSolved: false,
-    //         title: 'Title 1',
-    //         description: 'Description',
-    //         reporter: 'John Doe',
-    //         assignee: 'Jane',
-    //         date: '4.10.2021',
-    //         priority: 'Medium'
-    //     },
-    //     {
-    //         id: 'default3',
-    //         isSolved: true,
-    //         title: 'Title 2',
-    //         description: 'Description',
-    //         reporter: 'John Doe',
-    //         assignee: 'Jane',
-    //         date: '4.10.2021',
-    //         priority: 'High'
-    //     },
-    //     {
-    //         id: 'default4',
-    //         isSolved: false,
-    //         title: 'Title 2',
-    //         description: 'Description',
-    //         reporter: 'John Doe',
-    //         assignee: 'Jane',
-    //         date: '4.10.2021',
-    //         priority: 'High'
-    //     },
-    // ]);
 
     return (
         <TicketContext.Provider value={[tickets, setTickets]}>
